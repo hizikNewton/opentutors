a68ff7688dfe9d151a9982ef46bbf7bd8e8cabfc
bamidele-dell <bamideleisaac11@gmail.com>
2024-06-13 20:43:23 +0100

    added tutor service

--- tutor-nodb/src/routes.rs
+++ tutor-nodb/src/routes.rs
@@ -8,6 +8,7 @@ pub fn general_routes(cfg: &mut web::ServiceConfig) {
 
 
 pub fn course_routes(cfg:&mut web::ServiceConfig){
-    cfg.service(web::scope("/courses")
+    cfg
+        .service(web::scope("/courses")
         .route("/", web::post().to(new_course)));
 }
