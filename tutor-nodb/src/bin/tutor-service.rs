56a56920073449dd49b84501fd4bb9723a1402f2
Isaac Bamidele <isaac.bamidele@wayapaychat.com>
2024-06-14 08:14:15 +0100

    test get course for tutor

--- tutor-nodb/src/bin/tutor-service.rs
+++ tutor-nodb/src/bin/tutor-service.rs
@@ -14,20 +14,20 @@ use std::{io, sync::Mutex};
 
 use actix_web::{web, App, HttpServer};
 use routes::*;
-use state::Appstate;
+use state::AppState;
 
 #[actix_rt::main]
-async fn main()->io::Result<()> {
-    let shared_data = web::Data::new(Appstate {
+async fn main() -> io::Result<()> {
+    let shared_data = web::Data::new(AppState {
         health_check_response: "I'm good. You've already asked me ".to_string(),
         visit_count: Mutex::new(0),
         courses: Mutex::new(vec![]),
     });
     let app = move || {
         App::new()
-        .app_data(shared_data.clone())
-        .configure(general_routes)
-        .configure(course_routes);
+            .app_data(shared_data.clone())
+            .configure(general_routes)
+            .configure(course_routes)
     };
     HttpServer::new(app).bind("127.0.0.1:3000")?.run().await
 }
--- tutor-nodb/src/state.rs
+++ tutor-nodb/src/state.rs
@@ -1,8 +1,8 @@
-use std::sync::Mutex;
 use crate::models::Course;
+use std::sync::Mutex;
 
-pub struct Appstate{
-    pub health_check_response:String,
-    pub visit_count:Mutex<u32>,
-    pub courses:Mutex<Vec<Course>>
+pub struct AppState {
+    pub health_check_response: String,
+    pub visit_count: Mutex<u32>,
+    pub courses: Mutex<Vec<Course>>,
 }
